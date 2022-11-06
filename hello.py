bf6b8d33d57569521409bf874a034e89983d6e1a
Hamza <110835931+Ham-aziz@users.noreply.github.com>
2022-11-06 21:07:03 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# hello.py
+# hello.py 
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
-
 
 if __name__ == '__main__':
     app.run()
