079879a4afc05d02e12582906e78a2b2a8c3a943
Hamza <110835931+Ham-aziz@users.noreply.github.com>
2022-11-06 21:40:24 +0100

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,8 +1,9 @@
 # test_hello.py
 from hello import app
 
+
 def test_hello():
-    response = app.test_client().get('/')
+    response = app.test_client().get("/")
 
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.data == b"Hello, World!"
